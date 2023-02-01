e86b3fc3ee0c010cb7f0753f0bbf204e1ac7e801
yastewie <124162520+yastewie@users.noreply.github.com>
2023-02-01 15:07:57 +0200

    Add files via upload

---
+++ MyFirstApp/app/src/main/java/com/rytik/myfirstapp/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.rytik.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
